e0e4320f1f2ee2b63f0bc1241583aa01a8a76c4c
Sergio de Mingo <sdemingo@gmail.com>
2025-02-07 08:50:17 +0100

    no funciona

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # anexito
 
-Autorellenador de formulario PDF
+Autorellenador de campos de formularios en PDF
+
 
 ## Dependencias
 
-https://github.com/t-houssian/fillpdf
+Necesita la librería [fillpdf](https://github.com/t-houssian/fillpdf). Para instalarla usar `pip`:
+
+```
+pip install fillpdf
+```
+
+## Ejemplo de uso de la librería
+
+Para sobreescribir varios campos usar un diccionario como muestra el ejemplo:
+
+```
+data_dict = {
+'Campo1': 'valor del campo2',
+'Campo2': 'valor del campo2',
+}
+
+fillpdfs.write_fillable_pdf('formulario.pdf', 'nuevo.pdf', data_dict)
+
+```
+
+Para conseguir los nombres de los campos incluidos en el documento se puede usar el comando `extractfillpdf` incluido con la librería. Este comando genera un archivo JSON con todos los nombres.
+
+```
+$ extractfillpdf formulario.pdf
+```
+
+
+
+
--- anexito.py
+++ anexito.py
@@ -1,18 +1,49 @@
+#!/usr/bin/python3
+
 from fillpdf import fillpdfs
 
-fillpdfs.get_form_fields("formulario.pdf")
+import sys
 
+
+#fillpdfs.get_form_fields("formulario.pdf")
 # returns a dictionary of fields
+
 # Set the returned dictionary values a save to a variable
 # For radio boxes ('Off' = not filled, 'Yes' = filled)
 
 data_dict = {
-'Texto2': 'hooooooola',
-'Texto4': 'Adiooos',
-'Texto1': 'vamoooooooS',
+'Alumnoa': 'Sergio',
+'Check Box1': 'Yes',
+'Check Box2': 'Yes',
+'Check Box8': 'Yes',
+'Check Box4': 'Yes',
+'Check Box7': 'Yes',
+'Check Box48': 'Yes',
+'Check Box12': 'Yes',
+'Check Box16': 'Yes',
+'Check Box45.1.0': 'Yes',
 }
 
-fillpdfs.write_fillable_pdf('formulario.pdf', 'new.pdf', data_dict)
 
-# If you want it flattened:
-fillpdfs.flatten_pdf('formulario.pdf', 'newflat.pdf')
+if __name__=='__main__':
+
+    if (len(sys.argv)<2):
+        print ("Faltan argumentos")
+        sys.exit(0)
+
+    if (sys.argv[1] == "-l"):
+        fillpdfs.print_form_fields("formulario.pdf")
+        sys.exit(0)
+
+    if (sys.argv[1] == "-p"):
+        fillpdfs.write_fillable_pdf('formulario.pdf', 'new.pdf', data_dict)
+        # If you want it flattened:
+        #fillpdfs.flatten_pdf('formulario.pdf', 'newflat.pdf')
+
+
+    print ("Argumetos  erróneos")
+
+
+
+
+
