0af76edf47059490bfb2bddd56783b95d900419b
Sergio de Mingo <sdemingo@gmail.com>
2025-02-08 14:55:53 +0100

    agrego fecha en la firma

--- datos.py
+++ datos.py
@@ -4,12 +4,24 @@
 # Si quieres que alguno de estos campos salga en blanco usa una 
 # cadena vacia o ""
 
-codigo_ciclo="IFCS01"
-nombre_modulo="Implantación de Sistemas Informáticos"
-codigo_modulo="0369"
-nombre_ciclo="Administración de Sistemas Informáticos en Red"
-grupo="1º ASIR"
+## Datos de la cabecera
+
+codigo_ciclo="IFCS02"
+nombre_modulo="Informática aplicada a sistemas electrónicos"
+codigo_modulo="CMO311"
+nombre_ciclo="Desarrollo de Aplicaciones Multiplataforma"
+grupo="1º DAM B"
 curso="2024/2025"
 anio="2025"
-fecha="00/00/00"
+fecha_reunion="00/00/00"
+
+
+## Datos para la firma
+
+lugar_firma="Fuenlabrada"
+dia_firma="8"
+mes_firma="Febrero"
+anio_firma="2025"
+
+
 
